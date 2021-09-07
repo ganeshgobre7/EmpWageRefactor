5005e98f510fe34cd50db8b7e9cc664a87b4dac4
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-07 22:04:01 +0530

    Full time daily wage code added

--- EmployeeWageRef/Program.cs
+++ EmployeeWageRef/Program.cs
@@ -4,13 +4,18 @@ namespace EmployeeWageRef
 {
     public class calculate
     {
+        int full_Time = 8;
+        int per_Hour_Sal = 20;
+        int one_Day_Sal = 0;
+
         public void check()
         {
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if(empCheck==1)
             {
-                Console.WriteLine("Employee is Present");
+                one_Day_Sal = full_Time * per_Hour_Sal;
+                Console.WriteLine("Full Time Salary is:" + one_Day_Sal); ;
             }
             else
             {
