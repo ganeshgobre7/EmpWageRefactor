c50c050c5d385713908d55884ba4e5ff40811cd1
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-07 21:41:50 +0530

    Welcome message added

---
+++ EmployeeWageRef/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome to Employee Wage Computation Program With OOPS");
+        }
+    }
+}
