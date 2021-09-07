1d7df8c86c42525968ddeca45f4b25783b3d3070
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-07 21:53:58 +0530

    Employee present absent code added

--- EmployeeWageRef/Program.cs
+++ EmployeeWageRef/Program.cs
@@ -2,11 +2,29 @@
 
 namespace EmployeeWageRef
 {
+    public class calculate
+    {
+        public void check()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if(empCheck==1)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("WelCome to Employee Wage Computation Program With OOPS");
+            calculate cal = new calculate();
+            cal.check();
         }
     }
 }
